ca71294e54498bba113b0e4796808e0bc29df341
Ryan David Forsyth <forsyth.r@gmail.com>
2019-03-11 11:27:21 -0400

    Initial Commit

---
+++ UIView-Animations-Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIView-Animations-Practice
+//
+//  Created by Ryan David Forsyth on 2019-03-11.
+//  Copyright © 2019 Ryan F. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
